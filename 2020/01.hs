d2290152073e95aa16e7ca936c728a321343ddeb
prez <prez@national.shitposting.agency>
2020-12-03 22:40:53 +0100

    improve

--- 2020/01.hs
+++ 2020/01.hs
@@ -1,7 +1,6 @@
 module Main where
 
-import Control.Applicative
-import Text.Printf
+import Text.Printf ( printf )
 import Test.Hspec
 
 input :: IO [Integer]
@@ -10,20 +9,32 @@ input = map read <$> lines <$> readFile "inputs/01/input"
 main :: IO ()
 main = do
   nums <- input
-  printf "first:\t%d\nsecond:\t%d\n" (solve1 nums) (solve2 nums)
+  printf "first:\t%d\nsecond:\t%d\n" (solve 2 nums) (solve 3 nums)
 
-solve1, solve2 :: [Integer] -> Integer
-solve1 nums = uncurry (*) . head . filter (\(x,y) -> x+y==2020) $ liftA2 (,) nums nums
-solve2 nums = (\(x,y,z) -> x*y*z) . head $ [(x,y,z) | x<-nums, y<-nums, z<-nums, x+y+z == 2020]
+sample :: Integer -> [a] -> [[a]]
+sample 0 _ = [[]]
+sample _ [] = []
+sample n (x:xs) = map (x:) (sample (n-1) xs) <> sample n xs
+
+solve :: Integer -> [Integer] -> Integer
+solve = ((.).(.)) (product . head . filter ((==2020) . sum)) sample
 
 test :: IO ()
 test = hspec do
   describe "input" do
     it "1" do
-      solve1 [1721,979,366,299,675,1456] `shouldBe` 514579
+      solve 2 [1721,979,366,299,675,1456] `shouldBe` 514579
     it "2" do
-      solve2 [1721,979,366,299,675,1456] `shouldBe` 241861950
+      solve 3 [1721,979,366,299,675,1456] `shouldBe` 241861950
+    it "1h" do
+      solve 2 [1010,2000,20,10,18,2] `shouldBe` 2000*20
+    it "2h" do
+      solve 3 [1010,2000,20,10,18,2] `shouldBe` 2000*18*2
     it "silver" do
-      solve1 <$> input `shouldReturn` 988771
+      solve 2 <$> input `shouldReturn` 988771
     it "gold" do
-      solve2 <$> input `shouldReturn` 171933104
+      solve 3 <$> input `shouldReturn` 171933104
+    -- it "1-bigboy" do
+      -- (solve 99920044 2) <$> input "inputs/01/bigboy" `shouldReturn` 1939883877222459
+    -- it "2-bigboy" do
+      -- (solve 99920044 3) <$> input "inputs/01/bigboy" `shouldReturn` 32625808479480099854130
