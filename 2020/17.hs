6c136dfa92e5b610356bc1404d0673caab134286
prez <prez@national.shitposting.agency>
2020-12-18 14:39:54 +0100

    :3

--- 2020/17.hs
+++ 2020/17.hs
@@ -4,21 +4,19 @@ import Data.List ( transpose )
 import Text.Printf ( printf )
 import qualified Data.Set as Set
 
-input :: String -> IO [[Integer]]
-input f = do
-  rows <- zip [0..] <$> lines <$> readFile f
-  let cells = fmap (zip [0..] . map (=='#')) <$> rows
-  let cells' = concatMap (\(y,xs) -> uncurry (zip3 (repeat y)) $ unzip xs) cells
-  pure $ map fst . filter snd . map (\(x,y,s) -> ([x,y,0,0],s)) $ cells'
+input :: String -> IO (Set.Set [Int])
+input f = Set.fromList . concatMap r . zip [0..] . lines <$> readFile f
+  where
+    r (y,xs) = [[x,y,0,0] | (x,c)<-zip [0..] xs, c == '#']
 
-update :: Int -> Set.Set [Integer] -> Set.Set [Integer]
+update :: Int -> Set.Set [Int] -> Set.Set [Int]
 update n g = Set.fromList . mapMaybe (updcell g) . hull . Set.toList $ g
   where
-    neigh cell = zipWith (+) cell <$> (tail $ replicateM n [0,-1,1])
-    hull = sequenceA . map (liftM2 enumFromTo ((subtract 1) . minimum) ((+1) . maximum)) . transpose
-    updcell grid cell = rule cell (Set.member cell grid) (activeneigh cell)
+    neigh = (<$> (fmap (++[0 | n == 3]) . tail $ replicateM n [0,-1,1])) . zipWith (+)
+    hull = traverse (liftM2 enumFromTo (subtract 1 . minimum) ((+1) . maximum)) . transpose
+    updcell grid = liftM3 rule id (flip Set.member grid) activeneigh
       where
-        activeneigh = length . filter (==True) . map (flip Set.member grid) . neigh
+        activeneigh = length . filter (==True) . fmap (flip Set.member grid) . neigh
         rule c True 2 = Just c
         rule c True 3 = Just c
         rule _ True _ = Nothing
@@ -27,11 +25,6 @@ update n g = Set.fromList . mapMaybe (updcell g) . hull . Set.toList $ g
 
 main :: IO ()
 main = do
-  gridh <- input "inputs/17/input"
-  let grid = map (take 3) gridh
-  let sol1 = total 3 . f $ grid
-  let sol2 = total 4 . f $ gridh
-  printf "first:\t%d\nsecond:\t%d\n" sol1 sol2
+  input "inputs/17/input" >>= liftM2 (printf "first:\t%d\nsecond:\t%d\n") (total 3) (total 4)
   where
-    f = foldr Set.insert Set.empty
-    total n g = Set.size $ iterate (update n) g !! 6
+    total n = Set.size . (!!6) . iterate (update n) . foldr Set.insert Set.empty
